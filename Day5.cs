820f457d50294d76afd4a40e3814c39aaa2c6337
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-12-05 14:48:10 +0100

    an attempt for day 5/2

--- Day5.cs
+++ Day5.cs
@@ -27,7 +27,8 @@ public class Day5 : Day
             try
             {
                 var split = line.Split(" ").Select(long.Parse).ToList();
-                if (split.Count != 3) continue;
+                if (split.Count != 3)
+                    continue;
                 current ??= new Map();
                 current.Add(split[0], split[1], split[2]);
             }
@@ -49,7 +50,33 @@ public class Day5 : Day
 
     protected override object Part2(string path)
     {
-        throw new NotImplementedException();
+        var lines = Util.ReadFileLines(path);
+        var maps = GetMaps(lines).ToList();
+        var seeds = lines[0].Split(" ")[1..].Select(long.Parse).ToList();
+        if (seeds.Count % 2 != 0)
+            throw new FormatException();
+        var ranges = new List<(long, long)>();
+        for (var i = 0; i < seeds.Count; i += 2)
+        {
+            var start = seeds[i];
+            ranges.Add((start, start + seeds[i + 1]));
+        }
+        foreach (var map in maps)
+        {
+            var newRanges = new List<(long, long)>();
+            foreach (var range in ranges)
+            {
+                newRanges.AddRange(map.Get(range).Distinct());
+            }
+            ranges = newRanges;
+        }
+        return ranges.Select(e => e.Item1).Select(l =>
+        {
+            return maps.Aggregate(l, (current, map) =>
+            {
+                return map.Get(current);
+            });
+        }).Min();
     }
 }
 
@@ -60,6 +87,7 @@ internal class Map
     public void Add(long sourceStart, long destStart, long rangeLength)
     {
         _entries.Add(new Entry(sourceStart, destStart, rangeLength));
+        _entries.Sort();
     }
 
     public long Get(long i)
@@ -70,9 +98,27 @@ internal class Map
         }
         return i;
     }
+
+    public IEnumerable<(long, long)> Get((long, long) range)
+    {
+        var result = new List<(long, long)>();
+        foreach (var intersect in _entries.Select(e => e.Intersect(range)).Where(e => e.Item1 < e.Item2))
+        {
+            if (intersect.Item1 > range.Item1)
+            {
+                result.Add((range.Item1, intersect.Item1));
+            }
+            result.Add(intersect);
+            if (range.Item2 > intersect.Item2)
+            {
+                result.Add((intersect.Item2, range.Item2));
+            }
+        }
+        return result.Count == 0 ? new List<(long, long)> { range } : result;
+    }
 }
 
-internal class Entry
+internal class Entry : IComparable
 {
     private readonly long _sourceStart;
     private readonly long _destStart;
@@ -93,5 +139,16 @@ internal class Entry
     public long GetFor(long i)
     {
         return IsInRange(i) ? _sourceStart + i - _destStart : i;
+    }
+
+    public (long, long) Intersect((long, long) l)
+    {
+        return (Math.Max(_destStart, l.Item1), Math.Min(_destStart + _rangeLength, l.Item2));
+    }
+
+    int IComparable.CompareTo(object? o)
+    {
+        var e = (Entry) o;
+        return _destStart.CompareTo(e._destStart);
     }
 }
